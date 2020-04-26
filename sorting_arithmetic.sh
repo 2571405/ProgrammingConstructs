b8d98c7a5c10df8451d81c5a27acb95ff5bef60a
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-26 17:35:55 +0530

    added use case 6 (store in dictionary)

--- sorting_arithmetic.sh
+++ sorting_arithmetic.sh
@@ -12,3 +12,16 @@ second=$(( ($a*$b)+$c ))
 third=$(( $c+($a/$b) ))
 
 fourth=$(( ($a%$b)+$c ))
+
+echo "Result of 1st computation: "$first
+echo "Result of 2nd computation: "$second
+echo "Result of 3rd computation: "$third
+echo "Result of 4th computation: "$fourth
+
+declare -A dictionary
+dictionary[0]=$first
+dictionary[1]=$second
+dictionary[2]=$third
+dictionary[3]=$fourth
+
+
