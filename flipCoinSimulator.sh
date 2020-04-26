1807163a69323ed701f6b7dedb62c00e9267de3e
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-26 18:23:39 +0530

    display head or tail multiple times use case 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,3 +9,19 @@ then
 else
    echo "Tails"
 fi
+
+heads=0
+tails=0
+for (( i=1;i<=10;i=$i+1 ))
+do
+   toss1=$(( $RANDOM%2 ))
+   if (( $toss1 ))
+   then
+      heads=$(( $heads+1 ))
+   else
+      tails=$(( $tails+1 ))
+   fi
+done
+echo "The number of times HEADS won out of 10 times: "$heads
+echo "The number of times TAILS won out of 10 times: "$tails
+
